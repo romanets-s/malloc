ecd6840566514d3a38174bff2781ace3422a892f
Serhii <romanets.sv@gmail.com>
2019-04-09 19:48:42 +0300

    cmake-build-debug added

---
+++ cmake-build-debug/CMakeFiles/malloc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/malloc.dir/main.c.o"
+  "CMakeFiles/malloc.dir/malloc.c.o"
+  "malloc.pdb"
+  "malloc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/malloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
